b2fc0cabf1d63e52d11417f2a71a6bbcd3b2a5b3
Antonio Sánchez <asanchez@plutec.net>
2018-11-28 19:51:38 +0100

    Second version of environment

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,12 @@
 apt-get update
 apt-get install -y python3-pip
 apt-get install -y apache2
-pip install -r /vagrant/requirements.txt
+pip3 install --upgrade pip
+pip3 install -r /vagrant/requirements.txt
+
+cd /vagrant/ibanapp
+python3 manage.py migrate
+#python3 manage.py runserver 0.0.0.0:8000
 #if ! [ -L /var/www ]; then
 #  rm -rf /var/www
 #  ln -fs /vagrant /var/www
