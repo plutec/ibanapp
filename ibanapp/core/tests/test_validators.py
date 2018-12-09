05e3de0d8ce73ba5f44eabc8b553d1de52270950
Antonio Sánchez <asanchez@plutec.net>
2018-12-09 23:52:56 +0100

    Add another test way for validator

--- ibanapp/core/tests/test_validators.py
+++ ibanapp/core/tests/test_validators.py
@@ -13,3 +13,6 @@ class ValidatorTestCase(TestCase):
     def test_validator_iban_incorrect(self):
         with self.assertRaises(ValidationError):
             validate_iban("ES1416699352362824876005")  # Invalid IBAN
+
+        with self.assertRaises(ValidationError):
+            validate_iban("ES141669935236282487600Aasdasd")  # Invalid IBAN
